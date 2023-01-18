dfb274f13f20469033a67154ad980aaaf4aa799f
crafter312 <henrysquirewebb@gmail.com>
2023-01-17 18:00:13 -0600

    removed TODO item

--- src/main/java/net/thesquire/backroomsmod/portal/teleport/Level0Teleporter.java
+++ src/main/java/net/thesquire/backroomsmod/portal/teleport/Level0Teleporter.java
@@ -45,41 +45,4 @@ public class Level0Teleporter {
         return Optional.empty();
     }
 
-    //TODO add default portal creation function for new portal setup
-    /*
-    private static Pair<Optional<BlockLocating.Rectangle>, Direction> makeDestinationPortal(ServerWorld destWorld, BlockPos blockPos, BlockState frameBlock, Direction dir) {
-        WorldBorder worldBorder = destWorld.getWorldBorder();
-        PortalLink link = CustomPortalApiRegistry.getPortalLinkFromBase(frameBlock.getBlock());
-        PortalFrameTester portalFrameTester = link.getFrameTester().createInstanceOfPortalFrameTester();
-        Direction.Axis axis = Level0PortalAreaHelper.flipHorizontalAxis(dir.getAxis());
-
-        for (BlockPos.Mutable mutable : BlockPos.iterateInSquare(blockPos, 16, Direction.WEST, Direction.SOUTH)) {
-            BlockPos testingPos = mutable.toImmutable();
-            if (!worldBorder.contains(testingPos)) continue;
-
-            BlockPos pos = null;
-            if (canHoldPortal(destWorld.getBlockState(testingPos.add(0, -1, 0)))) {
-                BlockPos testRect = portalFrameTester.doesPortalFitAt(destWorld, testingPos, axis);
-                if (testRect != null) pos = testRect;
-            }
-
-            if (pos != null) {
-                portalFrameTester.createPortal(destWorld, pos, frameBlock, axis);
-                return new Pair<>(Optional.of(portalFrameTester.getRectangle()), dir);
-            }
-        }
-        portalFrameTester.createPortal(destWorld, blockPos, frameBlock, axis);
-        return new Pair<>(Optional.of(portalFrameTester.getRectangle()), dir);
-    }
-
-    private static boolean canHoldPortal(BlockState state) {
-        return state.getMaterial().isSolid();
-    }
-
-    protected static TeleportTarget idkWhereToPutYou(ServerWorld world, Entity entity, BlockPos pos) {
-        BackroomsMod.LOGGER.error("Unable to find tp location, forced to place on top of world");
-        BlockPos destinationPos = world.getTopPosition(Heightmap.Type.WORLD_SURFACE, pos);
-        return new TeleportTarget(new Vec3d(destinationPos.getX() + .5, destinationPos.getY(), destinationPos.getZ() + .5), entity.getVelocity(), entity.getYaw(), entity.getPitch());
-    }*/
-
 }
